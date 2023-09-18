b6ac6be2b292b99148ff699cd00bab75ecf0d505
TheKamsi <arinze_kamsiyochukwu@outlook.com>
2023-09-18 07:41:23 -0500

    Saved and added requirements.txt file

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import gradio as gr 
 from transformers import pipeline
+
+model = pipeline(task="image-classification", model="julien-c/hotdog-not-hodog")
+
+def predict(image):
+    predictions = model(image)
+    return {p["label"]: p["score"] for p in predictions}
+
+gr.Interface(
+    predict,
+    inputs=gr.Image(label="Upload image", type="filepath"),
+    outputs=gr.Label(num_top_classes=2),
+    title="Hotdog or not Hotdog"
+).launch()
